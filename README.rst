87354f1f536796dc061f786f76a417a3c992ee37
Caio Belfort <caiobelfort90@gmail.com>
2017-08-17 17:24:11 -0300

    Conexão com sql-server e postgresql

---
+++ README.rst
@@ -0,0 +1,6 @@
+Pacote para criar conexões com o banco de dados
+
+Dependencias:
+sqlalchemy
+pymssql para conexão com sql-server
+psycopg2 para conexão com postgresql
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='DBConnectionManager',
+    version='0.01',
+    description='My database connection libraries',
+    author='Caio Belfort',
+    author_email='caiobelfort90@gmail.com',
+    license='GPL',
+    packages=['DBConnectionManager'],
+    zip_safe=False
+)

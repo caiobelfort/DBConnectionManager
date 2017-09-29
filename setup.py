25cecfa95b634b3c01a427edf128f47405936d13
Caio Belfort <caiobelfort90@gmail.com>
2017-09-29 09:52:17 -0300

    change in setup.py python version tested

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3.*'
+        'Programming Language :: Python :: 3.6'
     ]
 )

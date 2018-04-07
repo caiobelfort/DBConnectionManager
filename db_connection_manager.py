ddd779825cf1889113fd1c3866c204cf16af71b5
Caio <caio.belfort@armateus.com.br>
2018-04-07 11:42:37 -0300

    Added function to return a cTDS connection

--- db_connection_manager.py
+++ db_connection_manager.py
@@ -28,7 +28,7 @@ def get_engine(connection_name: str,
                **kwargs
                ) -> engine.Engine:
     """
-    Returns an engine from a configuration file
+    Returns an sqlalchemy engine from a configuration file
     Args:
         connection_name: Name of connection
         config_file: Configuration file where the *connection_name* parameters are declared. Only JSON
@@ -36,17 +36,7 @@ def get_engine(connection_name: str,
         A SQLAlchemy engine
     """
 
-    # Loads the json data as dict
-    filename = config_file if config_file is not None else str(Path.home()) + '/.dbconnections.json'
-
-    with open(filename, 'r') as file:
-        connections = json.load(file)
-
-    # Check if connection name exists in configuration file
-    if connection_name not in connections:
-        raise ValueError("%s connection don't exists in %s" % (connection_name, filename))
-
-    conf = connections[connection_name]
+    conf = load_connection_config(config_file, connection_name)
 
     # Check if connection configuration have all required variables
     _check_required_attrs(conf, connection_name)
@@ -65,10 +55,48 @@ def get_engine(connection_name: str,
     return sa.create_engine(cstr, **kwargs)
 
 
+def load_connection_config(config_file, connection_name):
+    # Loads the json data as dict
+    filename = config_file if config_file is not None else str(Path.home()) + '/.dbconnections.json'
+    with open(filename, 'r') as file:
+        connections = json.load(file)
+    # Check if connection name exists in configuration file
+    if connection_name not in connections:
+        raise ValueError("%s connection don't exists in %s" % (connection_name, filename))
+    conf = connections[connection_name]
+    return conf
+
+
 def _check_required_attrs(connection_conf, connection_name):
     error_string = 'Required Attribute %s not declared in configuration file for connection %s.'
     required_attr = ('type', 'user', 'pwd', 'host', 'database')
     for v in required_attr:
         if v not in connection_conf:
             raise ValueError(error_string % (v, connection_name))
+
+
+def get_ctds_connection(connection_name: str, config_file: str = None):
+    """
+    Create a ctds connection from a connection define in a json file
+    Args:
+        connection_name: Name of connection
+        config_file: Name of file with defined connections
+    Returns:
+        ctds.Connection: A ctds connection
+    """
+    import ctds
+
+    conf = load_connection_config(config_file, connection_name)
+
+    if conf['type'] != 'mssql':
+        raise RuntimeError('%s is not a MSSQL connection.' % connection_name)
+
+    hostname = conf['host']
+    port = int(conf.get('port', '1433'))
+    user = conf['user']
+    pwd = conf['pwd']
+    database = conf['database']
+
+    conn = ctds.connect(hostname, port, user=user, password=pwd, database=database)
+    return conn
 

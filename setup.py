8710ae1d7ace007fafaa4f9738010d86d68cb70b
Caio <Belfort>
2018-08-15 16:30:09 -0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='db_connection_manager',
-    version='0.05a',
+    version='0.06',
     description='A project for management of connections of databases',
     author='Caio Belfort',
     author_email='caiobelfort90@gmail.com',
@@ -12,7 +12,10 @@ setup(
     install_requires=['sqlalchemy'],
     classifiers=[
         'Development Status :: 4 - Beta',
+        'Topic :: Software Development :: Database',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7',
+        'Operation System :: POSIX'
     ]
 )
